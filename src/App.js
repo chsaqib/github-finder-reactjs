6ea0d5e1c619696d823514d451fbd9319377505a
Saqib Nazir <saqib_9090@yahoo.com>
2019-10-21 21:27:25 +0200

    users component and some destrcturing...

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <UserItem />>
+      <div className="container">
+        <Users />
+      </div>
     </div>
   );
 }
