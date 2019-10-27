09d812488e180a83216c85aa9c47d9a73de7bfba
Saqib Nazir <saqib_9090@yahoo.com>
2019-10-27 03:15:13 +0100

    moving Repos state to Context..

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Fragment, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
-import axios from 'axios';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
@@ -11,21 +10,12 @@ import GithubState from './context/github/GithubState';
 import './App.css';
 
 const App = () => {
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
   // Get single user Data
 
   // Get users repos
-  const getUserRepos = async username => {
-    setLoading(true);
-    const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    setRepos(res.data);
-    setLoading(false);
-  };
+
   //Clear users from the state
 
   //
@@ -55,18 +45,7 @@ const App = () => {
                 )}
               />
               <Route exact path="/about" component={About}></Route>
-              <Route
-                exact
-                path="/user/:login"
-                render={props => (
-                  <User
-                    {...props}
-                    getUserRepos={getUserRepos}
-                    repos={repos}
-                    loading={loading}
-                  />
-                )}
-              ></Route>
+              <Route exact path="/user/:login" component={User}></Route>
             </Switch>
           </div>
         </div>
