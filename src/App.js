bf6c14196eaa1f4b8e681646e400f92ac1179a0a
Saqib Nazir <saqib_9090@yahoo.com>
2019-10-27 02:32:26 +0100

    reducer and actions are created

--- src/App.js
+++ src/App.js
@@ -17,15 +17,6 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
-  const searchUsers = async text => {
-    setLoading(true);
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    setUsers(res.data.items);
-    setLoading(false);
-  };
-
   // Get single user Data
   const getUser = async username => {
     setLoading(true);
@@ -71,7 +62,6 @@ const App = () => {
                 render={props => (
                   <Fragment>
                     <Search
-                      searchUsers={searchUsers}
                       clearUsers={clearUsers}
                       showClear={users.length > 0 ? true : false}
                       setAlert={showAlert}
--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -19,6 +19,16 @@ const GithubState = props => {
   };
   const [state, dispatch] = useReducer(GithubReducer, initialState);
   //search users
+  const searchUsers = async text => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data.items
+    });
+  };
 
   //get users
 
@@ -27,6 +37,7 @@ const GithubState = props => {
   //clear users
 
   //set loading
+  const setLoading = () => dispatch({ type: SET_LOADING });
 
   return (
     <GithubContext.Provider
@@ -34,7 +45,8 @@ const GithubState = props => {
         users: state.users,
         user: state.user,
         repos: state.repos,
-        loading: state.loading
+        loading: state.loading,
+        searchUsers
       }}
     >
       {props.children}
